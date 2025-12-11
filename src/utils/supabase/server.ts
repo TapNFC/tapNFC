1da17f06a234fe6ef2cddcce65f41ab2a6fabc36
code-huddle-kashif-kazmi <kashif.kazmi@code-huddle.com>
2025-12-11 18:15:03 +0500

    feat: build errors

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,3 +1,4 @@
+import type { CookieOptions } from '@supabase/ssr';
 import { createServerClient as createSupabaseServerClient } from '@supabase/ssr';
 
 // Create a version that doesn't use next/headers
@@ -8,14 +9,14 @@ export const createClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (_name) => {
+        get: (_name: string) => {
           // This will be replaced by the actual cookie in middleware
           return undefined;
         },
-        set: (_name, _value, _options) => {
+        set: (_name: string, _value: string, _options: CookieOptions) => {
           // This will be replaced by the actual cookie setter in middleware
         },
-        remove: (_name, _options) => {
+        remove: (_name: string, _options: CookieOptions) => {
           // This will be replaced by the actual cookie remover in middleware
         },
       },
